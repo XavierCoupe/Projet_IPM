3b8206d241891ae7388dd52f1f97ae7fd5055ad9
Xavier Coupe <xavier.coupe.etu@univ-lemans.fr>
2024-03-22 07:54:43 +0100

    modif vite.config et package.json

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "https://xaviercoupe.github.io/Projet_IPM/"
 })
